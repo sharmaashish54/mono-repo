fcce48e4e62e8bf618acf8a285b2921825347c36
ashishsharma <sharmaashish54@gmail.com>
2025-12-24 10:00:47 +0530

    node module -5

--- packages/module-a/index.js
+++ packages/module-a/index.js
@@ -1,2 +1,2 @@
 console.log('I am module-a');
-require('module-b');
+require('@monorepo-beginners/module-b');
--- packages/module-b/index.js
+++ packages/module-b/index.js
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-console.log('I am module-b dfsd');
+console.log('I am module-b');
